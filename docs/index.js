bf5ac7cfc3a72c7723145ad3c25a8fe3db590fd0
Travis Swiers <tswiers85@gmail.com>
2018-04-04 16:13:01 -0600

    transpiling to /docs folder

--- docs/index.js
+++ docs/index.js
@@ -8,6 +8,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+//testing more even more
 var heroMenu = {
   position: 'absolute',
   backgroundColor: 'black',
@@ -500,4 +501,4 @@ var Game = function (_React$Component) {
 }(React.Component);
 
 ReactDOM.render(React.createElement(Game, null), document.getElementById("rogueLike"));
-//# sourceMappingURL=index.js.map
+//# sourceMappingURL=../index.js.map
